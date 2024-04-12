5999c6285f6c10fb6d5e565a8b1ffce6fed4ed83
Filonych <e.filonych@gmail.com>
2024-04-12 17:37:59 +0300

    .

--- client/src/components/Pagination/index.jsx
+++ client/src/components/Pagination/index.jsx
@@ -1,59 +1,58 @@
-import { useDispatch, useSelector } from "react-redux";
-import * as SC from "./styles";
-import { changeFilter } from "../../redux/slices/filterSlice";
-import { useEffect, useState } from "react";
-import { getPosts } from "../../redux/slices/postsSlice";
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeFilter } from '../../redux/slices/filterSlice'
+import { getPosts } from '../../redux/slices/postsSlice'
+import * as SC from './styles'
 
-const ITEMS_PER_PAGE = 10;
+const ITEMS_PER_PAGE = 10
 
 export const Pagination = () => {
-  // добавила состояние pagination из-за useEffect
-  const [pagination, setPagination] = useState([]);
+	const [pagination, setPagination] = useState([])
 
-  const dispatch = useDispatch();
-  const { searchValue, currentPage, sort, order } = useSelector(
-    (state) => state.filter.filter
-  );
-  const totalCount = useSelector((state) => state.posts.totalCount);
-  const totalPages = Math.ceil(totalCount / ITEMS_PER_PAGE);
+	const dispatch = useDispatch()
+	const { searchValue, currentPage, sort, order } = useSelector(
+		state => state.filter.filter
+	)
+	const totalCount = useSelector(state => state.posts.totalCount)
 
-  // по-моему, итерация в useEffect срабатывает так же часто, как и без него
-  // ниже оба варианта для сравнения
+	// по-моему, итерация в useEffect срабатывает так же часто, как и без него
+	// ниже оба варианта для сравнения
 
-  // const pagination = [];
-  //   for (let i = 1; i <= totalPages; i++) {
-  //     pagination.push(i);
-  //   }
-  //   console.log("check pagination")
+	// const pagination = [];
+	//   for (let i = 1; i <= totalPages; i++) {
+	//     pagination.push(i);
+	//   }
+	//   console.log("check pagination")
 
-  useEffect(() => {
-    const pagination = [];
-    for (let i = 1; i <= totalPages; i++) {
-      pagination.push(i);
-    }
+	useEffect(() => {
+		const pagination = []
+		const totalPages = Math.ceil(totalCount / ITEMS_PER_PAGE)
+		for (let i = 1; i <= totalPages; i++) {
+			pagination.push(i)
+		}
 
-    // console.log("check pagination")
+		console.log('check pagination')
 
-    setPagination(pagination);
-  }, [totalPages]);
+		setPagination(pagination)
+	}, [totalCount])
 
-  const changeCurrentPage = (page) => {
-    const currentPage = page;
-    dispatch(changeFilter({ currentPage }));
-    dispatch(getPosts({ searchValue, currentPage, sort, order }));
-  };
+	const changeCurrentPage = page => {
+		const currentPage = page
+		dispatch(changeFilter({ currentPage }))
+		dispatch(getPosts({ searchValue, currentPage, sort, order }))
+	}
 
-  return (
-    <SC.Wrap>
-      {pagination.map((page) => (
-        <SC.Page
-          key={page}
-          onClick={() => changeCurrentPage(page)}
-          className={currentPage === page ? "active" : undefined}
-        >
-          {page}
-        </SC.Page>
-      ))}
-    </SC.Wrap>
-  );
-};
+	return (
+		<SC.Wrap>
+			{pagination.map(page => (
+				<SC.Page
+					key={page}
+					onClick={() => changeCurrentPage(page)}
+					className={currentPage === page ? 'active' : undefined}
+				>
+					{page}
+				</SC.Page>
+			))}
+		</SC.Wrap>
+	)
+}
--- client/src/components/Posts/components/Search/index.jsx
+++ client/src/components/Posts/components/Search/index.jsx
@@ -1,41 +1,32 @@
-import { useDispatch, useSelector } from "react-redux";
-import { changeFilter } from "../../../../redux/slices/filterSlice";
-import { Input } from "../../../ui/Input";
-import { useDebounce } from "../../../../hooks/useDebounce";
-import { getPosts } from "../../../../redux/slices/postsSlice";
-import { useCallback } from "react";
+import { useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useDebounce } from '../../../../hooks/useDebounce'
+import { changeFilter } from '../../../../redux/slices/filterSlice'
+import { getPosts } from '../../../../redux/slices/postsSlice'
+import { Input } from '../../../ui/Input'
 
 export const Search = () => {
-  const dispatch = useDispatch();
-  const { searchValue, sort, order } = useSelector(
-    (state) => state.filter.filter
-  );
+	const dispatch = useDispatch()
+	const { searchValue, sort, order } = useSelector(state => state.filter.filter)
 
-  const onChangeInput = (event) => {
-    const searchValue = event.target.value;
-    dispatch(changeFilter({ searchValue }));
-    debouncedUpdatePosts(searchValue);
-  };
+	const onChangeInput = event => {
+		const searchValue = event.target.value
+		dispatch(changeFilter({ searchValue }))
+		debouncedUpdatePosts(searchValue)
+	}
 
-  const updatePage = (searchValue) => {
-    const currentPage = 1;
-    dispatch(changeFilter({ currentPage }));
-    dispatch(getPosts({ searchValue, currentPage, sort, order }));
-  };
+	const updatePage = searchValue => {
+		const currentPage = 1
+		dispatch(changeFilter({ currentPage }))
+		dispatch(getPosts({ searchValue, currentPage, sort, order }))
+	}
 
-  // без useCallback не работает debounce, т.е. сколько знаков ввожу в инпут -
-  // столько запросов на сервер.
-  // ниже 2 варианта - с useCallback и без.
-  // я так поняла, что ты имела в виду, что надо сделать без useCallback?
+	const debouncedUpdatePosts = useCallback(useDebounce(updatePage, 500), [])
 
-  const debouncedUpdatePosts = useCallback(useDebounce(updatePage, 500), []);
-
-  // const debouncedUpdatePosts = useDebounce(updatePage, 500)
-
-  return (
-    <div>
-      <h3>Поиск</h3>
-      <Input type="text" value={searchValue} onChange={onChangeInput} />
-    </div>
-  );
-};
+	return (
+		<div>
+			<h3>Поиск</h3>
+			<Input type='text' value={searchValue} onChange={onChangeInput} />
+		</div>
+	)
+}
